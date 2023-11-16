16c16e9000966abf68980da44e59dd4dfd3ae334
Guilherme Beltrame Munhoz <gbeltramem@gmail.com>
2023-11-15 21:07:14 -0300

    cadAlu

--- js/ConfeDiaAlu.js
+++ js/ConfeDiaAlu.js
@@ -1,43 +1,50 @@
-
 function seSelecionado(){
     const Segunda = formCadAlu.Segunda();
     const Terca = formCadAlu.Terca();
     const Quarta = formCadAlu.Quarta();
     const Quinta = formCadAlu.Quinta();
     const Sexta = formCadAlu.Sexta();
+    const SegundaEntrada = formCadAlu.SegundaEntrada();
+    const SegundaSaida = formCadAlu.SegundaSaida();
+    const TercaEntrada = formCadAlu.TercaEntrada();
+    const TercaSaida = formCadAlu.TercaSaida();
+    const QuartaEntrada = formCadAlu.QuartaEntrada();
+    const QuartaSaida = formCadAlu.QuartaSaida();
+    const QuintaEntrada = formCadAlu.QuintaEntrada();
+    const QuintaSaida = formCadAlu.QuintaSaida();
+    const SextaEntrada = formCadAlu.SextaEntrada();
+    const SextaSaida = formCadAlu.SextaSaida();
     if(Segunda.checked){
         Segunda.value = 1;
-        console.log("Seegunda ="+Segunda.value)
+        
     }else{
         Segunda.value = 0;
-        console.log("Seegunda = "+Segunda.value)
     }
     if(Terca.checked){
         Terca.value = 1;
-        console.log("Terca ="+Terca.value)
     }else{
         Terca.value = 0;
-        console.log("Terca = "+Terca.value)
     }
     if(Quarta.checked){
         Quarta.value = 1;
-        console.log("Quarta ="+Quarta.value)
     }else{
         Quarta.value = 0;
-        console.log("Quarta = "+Quarta.value)
     }
     if(Quinta.checked){
         Quinta.value = 1;
-        console.log("Quinta ="+Quinta.value)
     }else{
         Quinta.value = 0;
-        console.log("Quinta = "+Quinta.value)
     }
     if(Sexta.checked){
         Sexta.value = 1;
-        console.log("Sexta ="+Sexta.value)
     }else{
         Sexta.value = 0;
-        console.log("Sexta = "+Sexta.value)
     }
 }
+
+
+const diasSemana = [{
+    1 : {
+        Segunda
+    }
+}]
